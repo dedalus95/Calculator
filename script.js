74a8d1175e1b0e93617458c75d242aebefb4f0fe
Emanuele <ecerilli1@gmail.com>
2021-07-14 11:18:59 +0200

    nope

--- script.js
+++ script.js
@@ -20,6 +20,25 @@ const zero = document.querySelector('#zero');
 const clear = document.querySelector('#clear');
 
 
+const operators = document.querySelectorAll('#operators button');
+operators.forEach(button => 
+    button.addEventListener('mousedown', function(e) {
+        if(e.target === plus) {
+            display.textContent = display.textContent.concat('+');
+        }
+        if(e.target === minus) {
+            display.textContent = display.textContent.concat('-');
+        }
+        if(e.target === per) {
+            display.textContent = display.textContent.concat('x');
+        }
+        if(e.target === division) {
+            display.textContent = display.textContent.concat(':');
+        }
+    
+    }));
+
+
 const digits = document.querySelectorAll('#digits button');
 digits.forEach(button => 
     button.addEventListener('mousedown', function(e) {
@@ -64,158 +83,220 @@ digits.forEach(button =>
      {display.textContent = display.textContent.concat(e.key);}
  });
 
-
-
-// addition code
+//try experiment
 
 let firstNumber;
 let secondNumber;
 
-plus.addEventListener('mousedown', function() {
-    firstNumber = display.textContent;
-    console.log('firstNumber is ' + firstNumber);
 
-    if(plus) {
-        display.textContent = '';
-        
+
+function a() {
+    let a = display.textContent;
+
+    if (plus) {
+        b = display.textContent;
+        return display.textContent = operate(a,plus,b);
+    }
+}
+
+
+
+
+
+
+
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function operate(a, operator, b) {
+    
+    if (operator == plus) {
+        return add(a,b);
     }
 
-    function operate() {
-        secondNumber = display.textContent;
-        equal.addEventListener('mousedown', function(){
-            secondNumber = display.textContent;
-            // console.log('secondNumber is ' + secondNumber);
-            function add() {
-                console.log('first number is ' + firstNumber, 'second number is ' + secondNumber);
-                display.textContent = '';
-                 display.textContent = parseInt(firstNumber) + parseInt(secondNumber);
-                 if(clear) {
-                    firstNumber = 0;
-                    secondNumber = 0;
+    if (operator == minus) {
+        return subtract(a,b);
+    }
 
-                 }
+    if(operator == per) {
+        return multiply(a,b);
+    }
+
+    if(operator == division) {
+        return divide(a,b);
+    }
+}
+
+
+
+
+
+
+
+
+// addition code
+
+// let firstNumber;
+// let secondNumber;
+
+// plus.addEventListener('mousedown', function() {
+//     firstNumber = display.textContent;
+//     console.log('firstNumber is ' + firstNumber);
+
+//     if(plus) {
+//         display.textContent = '';
+        
+//     }
+
+//     function operate() {
+//         secondNumber = display.textContent;
+//         equal.addEventListener('mousedown', function(){
+//             secondNumber = display.textContent;
+//             // console.log('secondNumber is ' + secondNumber);
+//             function add() {
+//                 console.log('first number is ' + firstNumber, 'second number is ' + secondNumber);
+//                 display.textContent = '';
+//                  display.textContent = parseInt(firstNumber) + parseInt(secondNumber);
+//                  if(clear) {
+//                     firstNumber = 0;
+//                     secondNumber = 0;
+
+//                  }
 
                  
-            }
-            add();
-        });
+//             }
+//             add();
+//         });
 
-    }
-    operate();
-});
+//     }
+//     operate();
+// });
 
 
 
-// multiplication code
+// // multiplication code
 
-let firstNumber1 ;
-let secondNumber1 ;
+// let firstNumber1 ;
+// let secondNumber1 ;
 
-per.addEventListener('mousedown', function() {
-    firstNumber = display.textContent;
-    // console.log('firstNumber is ' + firstNumber1);
+// per.addEventListener('mousedown', function() {
+//     firstNumber = display.textContent;
+//     // console.log('firstNumber is ' + firstNumber1);
 
-    if(per) {
-        display.textContent = '';
+//     if(per) {
+//         display.textContent = '';
         
-    }
+//     }
 
-    function operate() {
-        // secondNumber1 = display.textContent;
-        // console.log(secondNumber1);
-        equal.addEventListener('mousedown', function(){
-            secondNumber = display.textContent;
-            // console.log('secondNumber is ' + secondNumber1);
-            function multiply() {
-                console.log('first number is ' + firstNumber, 'second number is ' + secondNumber);
-                display.textContent = '';
-                let result = parseInt(firstNumber) * parseInt(secondNumber);
-                display.textContent = result;
-                console.log('result is '+ result);
+//     function operate() {
+//         // secondNumber1 = display.textContent;
+//         // console.log(secondNumber1);
+//         equal.addEventListener('mousedown', function(){
+//             secondNumber = display.textContent;
+//             // console.log('secondNumber is ' + secondNumber1);
+//             function multiply() {
+//                 console.log('first number is ' + firstNumber, 'second number is ' + secondNumber);
+//                 display.textContent = '';
+//                 let result = parseInt(firstNumber) * parseInt(secondNumber);
+//                 display.textContent = result;
+//                 console.log('result is '+ result);
                  
-                //  secondNumber1 = firstNumber1;
-                //  if(clear) {
-                //     secondNumber1 = firstNumber1;                
-                //      }
-                 }
-            multiply();
-        });
+//                 //  secondNumber1 = firstNumber1;
+//                 //  if(clear) {
+//                 //     secondNumber1 = firstNumber1;                
+//                 //      }
+//                  }
+//             multiply();
+//         });
 
-    }
-    operate();
-});
+//     }
+//     operate();
+// });
 
 
-//SUBTRACTION CODE 
+// //SUBTRACTION CODE 
 
-minus.addEventListener('mousedown', function() {
-    firstNumber = display.textContent;
-    console.log('firstNumber is ' + firstNumber);
+// minus.addEventListener('mousedown', function() {
+//     firstNumber = display.textContent;
+//     console.log('firstNumber is ' + firstNumber);
 
-    if(minus) {
-        display.textContent = '';
+//     if(minus) {
+//         display.textContent = '';
         
-    }
+//     }
 
-    function operate() {
-        secondNumber = display.textContent;
-        equal.addEventListener('mousedown', function(){
-            secondNumber = display.textContent;
-            // console.log('secondNumber is ' + secondNumber);
-            function subtract() {
-                console.log('first number is ' + firstNumber, 'second number is ' + secondNumber);
-                display.textContent = '';
-                 display.textContent = parseInt(firstNumber) - parseInt(secondNumber);
-                 if(clear) {
-                    firstNumber = 0;
-                    secondNumber = 0;
+//     function operate() {
+//         secondNumber = display.textContent;
+//         equal.addEventListener('mousedown', function(){
+//             secondNumber = display.textContent;
+//             // console.log('secondNumber is ' + secondNumber);
+//             function subtract() {
+//                 console.log('first number is ' + firstNumber, 'second number is ' + secondNumber);
+//                 display.textContent = '';
+//                  display.textContent = parseInt(firstNumber) - parseInt(secondNumber);
+//                  if(clear) {
+//                     firstNumber = 0;
+//                     secondNumber = 0;
 
-                 }
-
-                 
-            }
-            subtract();
-        });
-
-    }
-    operate();
-});
-
-
-//DIVISION CODE
-
-division.addEventListener('mousedown', function() {
-    firstNumber = display.textContent;
-    console.log('firstNumber is ' + firstNumber);
-
-    if(division) {
-        display.textContent = '';
-        
-    }
-
-    function operate() {
-        secondNumber = display.textContent;
-        equal.addEventListener('mousedown', function(){
-            secondNumber = display.textContent;
-            // console.log('secondNumber is ' + secondNumber);
-            function divide() {
-                console.log('first number is ' + firstNumber, 'second number is ' + secondNumber);
-                display.textContent = '';
-                 display.textContent = parseInt(firstNumber) / parseInt(secondNumber);
-                 if(clear) {
-                    firstNumber = 0;
-                    secondNumber = 0;
-
-                 }
+//                  }
 
                  
-            }
-            divide();
-        });
+//             }
+//             subtract();
+//         });
 
-    }
-    operate();
-});
+//     }
+//     operate();
+// });
+
+
+// //DIVISION CODE
+
+// division.addEventListener('mousedown', function() {
+//     firstNumber = display.textContent;
+//     console.log('firstNumber is ' + firstNumber);
+
+//     if(division) {
+//         display.textContent = '';
+        
+//     }
+
+//     function operate() {
+//         secondNumber = display.textContent;
+//         equal.addEventListener('mousedown', function(){
+//             secondNumber = display.textContent;
+//             // console.log('secondNumber is ' + secondNumber);
+//             function divide() {
+//                 console.log('first number is ' + firstNumber, 'second number is ' + secondNumber);
+//                 display.textContent = '';
+//                  display.textContent = parseInt(firstNumber) / parseInt(secondNumber);
+//                  if(clear) {
+//                     firstNumber = 0;
+//                     secondNumber = 0;
+
+//                  }
+
+                 
+//             }
+//             divide();
+//         });
+
+//     }
+//     operate();
+// });
 
 
 // function add() {
